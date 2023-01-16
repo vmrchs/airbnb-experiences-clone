88a32a7d40dec9ec874c5ea6b85edf677acf5065
vmrchs <vmrchs@gmail.com>
2023-01-16 14:49:25 -0300

    implementing props for the very first time

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
 import "./App.css";
+import RodneyMullen from "./assets/rodney-mullen.png";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,7 +12,14 @@ function App() {
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        img={RodneyMullen}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with R0dney Mullen"
+        price={136}
+      />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import RodneyMullen from "../assets/rodney-mullen.png";
 import Star from "../assets/star.png";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
-      <img src={RodneyMullen} alt="" className="card--image" />
+      <img src={props.img} className="card--image" />
       <div className="card--stats">
         <img src={Star} alt="" className="card--star" />
-        <span>5.0</span>
-        <span className="gray"> (6) • </span>
-        <span className="gray">USA</span>
+        <span>{props.rating}</span>
+        <span className="gray"> ({props.reviewCount}) • </span>
+        <span className="gray">{props.country}</span>
       </div>
-      <p>Life Lessons with Rodney Mullen</p>
+      <p>{props.title}</p>
       <p>
-        <span className="bold">From $136</span> / person
+        <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
   );
